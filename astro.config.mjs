33c33ad2ec0298603618a2eaddb3e049346f1663
mcinotti <12519127+MCinotti@users.noreply.github.com>
2025-01-18 10:25:15 -0500

    comented out starlight svelte and modified netlify import

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,16 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import robotsTxt from "astro-robots-txt";
-import netlify from "@astrojs/netlify/functions";
+import netlify from "@astrojs/netlify";
 // import netlify from '@astrojs/netlify/edge-functions'; no compat with builders below
-import svelte from "@astrojs/svelte";
+//import svelte from "@astrojs/svelte";
 
-import starlight from '@astrojs/starlight';
+//import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://magicpuddle.netlify.app',
-  integrations: [mdx(), sitemap(), robotsTxt(), svelte(), starlight()],
+  integrations: [mdx(), sitemap(), robotsTxt()],
   output: 'server',
   publish: 'dist',
   adapter: netlify({

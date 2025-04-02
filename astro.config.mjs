524d138d0e072a51a81c1f5b0ecfcebf4ba7e23b
mcinotti <12519127+MCinotti@users.noreply.github.com>
2025-04-01 21:48:18 -0400

    just looking around again

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import robotsTxt from "astro-robots-txt";
 import netlify from "@astrojs/netlify";
-// import netlify from '@astrojs/netlify/edge-functions'; no compat with builders below
-//import svelte from "@astrojs/svelte";
-
+// import netlify from '@astrojs/netlify/edge-functions'; // no compat with builders below
 //import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
